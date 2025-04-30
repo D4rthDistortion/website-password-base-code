747e6aa2e20485d3163b3ad9b3fb5eb99b08cf06
D4rthDistortion <stephenwhitley07@gmail.com>
2025-04-30 02:59:56 +0000

    commit apr 29 for moving all edits to the main repository

--- index.js
+++ index.js
@@ -7,31 +7,30 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-var userIsAuthorised = false;
-
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 function passwordCheck(req, res, next) {
     const password = req.body["password"];
     if (password === "ILoveProgramming") {
-        userIsAuthorised = true;
+        req.userIsAuthorised = true; // Store authorization status in the request object
+    } else {
+        req.userIsAuthorised = false;
     }
     next();
 }
-app.use (passwordCheck);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/check", (req, res) => {
-    if (userIsAuthorised) {
+app.post("/check", passwordCheck, (req, res) => {
+    if (req.userIsAuthorised) {
         res.sendFile(__dirname + "/public/secret.html");
-  } else {
-    res.sendFile(__dirname + "/public/index.html");
+    } else {
+        res.sendFile(__dirname + "/public/index.html");
     }
 });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-  });
+});
